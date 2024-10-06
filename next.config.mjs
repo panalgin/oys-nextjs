915c28e9a78396a4480edfab93ec4049e8df1133
Mustafa YILDIZ <mashadow@outlook.com>
2024-10-06 20:29:59 +0300

    jwt payload signed, server environment variables seperated from .env.local

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,22 @@
+import { loadEnvConfig } from '@next/env';
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const serverEnvPath = path.join(process.cwd(), '.env.server');
+
+let serverEnv = {};
+if (fs.existsSync(serverEnvPath)) {
+	serverEnv = dotenv.parse(fs.readFileSync(serverEnvPath));
+	Object.keys(serverEnv).forEach(key => {
+		if (key.startsWith('NEXT_SERVER_')) {
+			process.env[key] = serverEnv[key];
+		}
+	});
+}
+
+loadEnvConfig(process.cwd());
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
@@ -8,6 +27,9 @@ const nextConfig = {
 			},
 		],
   },
+  serverRuntimeConfig: Object.fromEntries(
+    Object.entries(serverEnv).filter(([key]) => key.startsWith('NEXT_SERVER_'))
+  ),
 };
 
 export default nextConfig;
--- src/app/api/auth/sign-in/route.ts
+++ src/app/api/auth/sign-in/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { firestore } from '@/server/firebase-admin';
 import { UserSignInDto } from '@/common/user-sign-in.dto';
+import jwt from 'jsonwebtoken';
+import { Role } from '../../../../common/roles.enum';
 
 export async function POST(req: Request) {
 	const { uid, email, displayName, photoURL } = await req.json();
@@ -20,7 +22,43 @@ export async function POST(req: Request) {
 		const userRef = firestore.collection('users').doc(uid);
 
 		await userRef.set(userSignInDto);
-		return NextResponse.json({ message: 'User upserted', userId: userRef.id }, { status: 200 });
+
+		const payload: jwt.JwtPayload = {
+			sub: uid,
+			aud: 'okul-app',
+			roles: [Role.User],
+			email: email,
+			displayName: displayName,
+			photoURL: photoURL,
+			iat: Math.floor(Date.now() / 1000),
+			exp: Math.floor(Date.now() / 1000) + 86400, // 1 day in seconds
+			iss: 'okul-app',
+		};
+
+		const jwtSecret = process.env.NEXT_SERVER_JWT_SECRET;
+		
+		if (!jwtSecret) {
+			throw new Error('JWT_SECRET is not set');
+		}
+		// Create JWT token
+		const token = jwt.sign(payload, jwtSecret, { expiresIn: '1d' });
+
+		// Create response with cookie
+		const response = NextResponse.json(
+			{ message: 'User upserted', userId: userRef.id },
+			{ status: 200 }
+		);
+
+		// Set cookie
+		response.cookies.set('_okup_app_jwt', token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			sameSite: 'strict',
+			maxAge: payload.exp && payload.iat ? payload.exp - payload.iat : 86400,
+			path: '/',
+		});
+
+		return response;
 
 	} catch (error) {
 		if (error instanceof Error) {
